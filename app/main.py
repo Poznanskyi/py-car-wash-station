71fce31aa9c88df6cd2a8c8eacd7c11a74632a4e
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-07-25 12:28:39 +0300

    Task 5 fix-2

--- app/main.py
+++ app/main.py
@@ -2,21 +2,43 @@ from typing import List
 
 
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
+        if comfort_class not in range(1, 11):
+            raise ValueError("Must be between 1 and 10")
+
+        if clean_mark not in range(0, 11):
+            raise ValueError("Must be between 0 and 10")
+
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
+        if distance_from_city_center <= 0:
+            raise ValueError("Must be greater than 0")
+
+        if clean_power not in range(0, 11):
+            raise ValueError("Must be between 0 and 10")
+
+        if not (0.0 <= average_rating <= 5.0):
+            raise ValueError("Must be between 0.0 and 5.0")
+
+        if count_of_ratings < 0:
+            raise ValueError("Must be non-negative")
+
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
